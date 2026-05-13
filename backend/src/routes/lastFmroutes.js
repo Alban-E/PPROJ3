801c189b45fab67a65b8031d0253ef87ad65e863
Alban-E <alban.evrard@supinfo.com>
2026-05-14 01:31:23 +0200

    Set up api call routes

--- backend/src/routes/lastFmroutes.js
+++ backend/src/routes/lastFmroutes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
 const { authenticateToken, assertUserIsAdmin } = require('../Middlewares/authVerif')
-const { searchTracks } = require('../Controllers/lastFmController')
+const { searchTrack, searchArtist, searchArtistTracks, searchArtistAlbums, searchAlbum, searchAlbumTags } = require('../Controllers/lastFmController')
 
-router.get('/tracks', searchTracks)
+router.get('/tracks', searchTrack)
+
+router.get('/artist', searchArtist)
+router.get('/artist/tracks', searchArtistTracks)
+router.get('/artist/album', searchArtistAlbums)
+
+router.get('/album', searchAlbum)
+router.get('/album/tags', searchAlbumTags)
 
 module.exports = router

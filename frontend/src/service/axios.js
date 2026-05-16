60e6301825d3afca955d06656728d8c30e13090a
Alban-E <alban.evrard@supinfo.com>
2026-05-16 22:11:54 +0200

    set up axios routes

--- frontend/src/service/axios.js
+++ frontend/src/service/axios.js
@@ -17,8 +17,80 @@ axiosInstance.interceptors.response.use((response) =>{
         return Promise.reject(error);
     });
 
+
+// #region Authentication part 
+
 export const register = async (payload) => {
     const res = await axiosInstance.post('/user/register', payload)
     return res.data
 }
 
+export const login = async (payload) => {
+    const res = await axiosInstance.post('/user/login', payload) 
+    return res.data
+}
+//#endregion
+
+
+//#region User
+//#endregion
+
+
+//#region Lists
+//#endregion
+
+
+
+
+
+//#region Tracks
+//#endregion
+
+
+
+
+
+//#region Feedback
+//#endregion
+
+
+
+
+
+//#region Feedback Comment
+//#endregion
+
+
+
+
+
+//#region Notification
+//#endregion
+
+
+
+
+
+//#region Subscription
+//#endregion
+
+
+
+
+
+//#region Chat
+//#endregion
+
+
+
+
+
+//#region Moderation
+//#endregion
+
+
+
+
+
+//#region API
+//#endregion

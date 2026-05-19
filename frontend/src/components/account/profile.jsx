e8d2aa56e4d690739cf7c3cc7843b5b850ec37e7
Alban-E <alban.evrard@supinfo.com>
2026-05-19 17:35:44 +0200

    Display the username on the profile page

--- frontend/src/components/account/profile.jsx
+++ frontend/src/components/account/profile.jsx
@@ -1,14 +1,16 @@
 import { useAuth } from "../../service/AuthContext"
 
 export default function Profile() {
-  const user = useAuth()
-  const { logout } = useAuth()
+  const { user, logout } = useAuth()
 
   return (
-    <div>
-      <h1>Profil</h1>
-      <p>Bienvenue {user.usernam}</p>
-      <button onClick={logout}>Se déconnecter</button>
-    </div>
+    <>
+      <div>
+        <h1>Profil</h1>
+        <h2>Bienvenue {user.username}</h2>
+        <p>Modifier les données etc etc etc</p>
+        <button onClick={logout}>Se déconnecter</button>
+      </div>
+    </>
   )
 }

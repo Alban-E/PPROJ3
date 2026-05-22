ae53026a4e7a43a5d30d012035fbbf756b33be33
Alban-E <alban.evrard@supinfo.com>
2026-05-22 10:42:34 +0200

    set up playlist tracks display + Track Item

--- frontend/src/pages/Playlists/playlistDetails.jsx
+++ frontend/src/pages/Playlists/playlistDetails.jsx
@@ -2,6 +2,8 @@ import { useSearchParams } from "react-router-dom";
 import { getListById, getTracksFromList } from "../../service/axios";
 import { useEffect, useState } from "react";
 import TrackItem from "../../components/TrackItem";
+import styles from './playlistDetails.module.css'
+import { Fragment } from "react";
 
 export default function PlayListDetails() {
     const [searchParams]  = useSearchParams()
@@ -10,6 +12,27 @@ export default function PlayListDetails() {
     const [tracks, setTracks] = useState([])
     const [loading, setLoading] = useState(true)
     const [authorized, setAuthorized] = useState(true); 
+    const test = [
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+        {name: "Rap god", releaseDate: "01/01/1999", imageUrl: "https://cdn-images.dzcdn.net/images/cover/207567487b1e79e1c530e2f4004ef287/1900x1900-000000-80-0-0.jpg", artist: "Eminem", apiId: "1"},
+    ]
 
     const updateTracks = async() => {
         try {
@@ -39,17 +62,18 @@ export default function PlayListDetails() {
     },[])
     
     return (
-        <>
-            {authorized ?
-                <>
-                    <p>{list.name}</p>
-                    {tracks.map((track, index) => (
-                    <TrackItem track={track} ></TrackItem>
+        authorized ?
+            <div className={styles.content}>
+                <h2 className={styles.title}>{list.name}</h2>
+                <p>Morceaux présents dans la playlists :</p>
+                <div className={styles.trackList}>
+                    {test.map((track, index) => (
+                    <TrackItem key={index} track={track} ></TrackItem>
                     ))}
-                </>
-            :
-                <p>Vous n'êtes pas autorisé à acceder à cette playlist</p>
-            }
-        </>
+                </div>
+            </div>
+        :
+            <p>Vous n'êtes pas autorisé à acceder à cette playlist</p>
+        
     )
 }
